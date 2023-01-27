0efbf205df1b1e90982dc9c5f4fd0d3c2fba4786
flufl <flufftehgamer@gmail.com>
2023-01-27 17:01:15 +0700

    fix: package not working for TS

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,8 +1,22 @@
-export * as animal from "./endpoints/animal";
-export * as animu from "./endpoints/animu";
-export * as canvas from "./endpoints/canvas";
-export * as others from "./endpoints/others";
+import * as animal from "./endpoints/animal";
+import * as animu from "./endpoints/animu";
+import * as canvas from "./endpoints/canvas";
+import * as others from "./endpoints/others";
 export * from "./typings/Animals";
 export * from "./typings/Animu";
 export * from "./typings/Canvas";
 export * from "./typings/Others";
+declare const _default: {
+    /** Animal Endpoints */
+    animal: typeof animal;
+
+    /** Animu Endpoints */
+    animu: typeof animu;
+
+    /** Canvas Endpoints */
+    canvas: typeof canvas;
+
+    /** Others Endpoints */
+    others: typeof others;
+};
+export default _default;
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
-export * as animal from "./endpoints/animal";
-export * as animu from "./endpoints/animu";
-export * as canvas from "./endpoints/canvas";
-export * as others from "./endpoints/others";
-
+import * as animal from "./endpoints/animal";
+import * as animu from "./endpoints/animu";
+import * as canvas from "./endpoints/canvas";
+import * as others from "./endpoints/others";
 
 export * from "./typings/Animals";
 export * from "./typings/Animu";
 export * from "./typings/Canvas";
 export * from "./typings/Others";
+
+export default { animal, animu, canvas, others, };
