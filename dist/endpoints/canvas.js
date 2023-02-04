86d414550eda3e74a94dcd3a983fc743aca0125c
fuwaguwa <flufftehgamer@gmail.com>
2023-02-04 17:20:13 +0700

    temporal removal

--- dist/endpoints/canvas.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.misc = exports.filter = exports.overlay = void 0;
-const tslib_1 = require("tslib");
-exports.overlay = tslib_1.__importStar(require("./canvas/overlay"));
-exports.filter = tslib_1.__importStar(require("./canvas/filter"));
-exports.misc = tslib_1.__importStar(require("./canvas/misc"));
